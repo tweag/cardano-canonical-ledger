70496e69430c2eb31ee5d43f28682e71b734bdab
João Santos Reis <joao.reis@moduscreate.com>
2025-09-03 09:47:58 +0100

    Remove cross compilation comment from template

--- nix/hix.nix
+++ nix/hix.nix
@@ -2,14 +2,6 @@
   name = "cardano-canonical-ledger";
   compiler-nix-name = "ghc910"; # Version of GHC to use
 
-  # Cross compilation support:
-  # crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
-  #   p.mingwW64
-  #   p.ghcjs
-  # ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
-  #   p.musl64
-  # ]);
-
   # Tools to include in the development shell
   shell.tools = {
     cabal = "latest";
