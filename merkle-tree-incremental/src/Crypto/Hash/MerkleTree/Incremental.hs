e5d633609e0b3c61721dbb4db14e1ba8a3ddc363
João Santos Reis <joao.reis@tweag.io>
2025-09-08 17:25:31 +0100

    Simplify external API

--- merkle-tree-incremental/src/Crypto/Hash/MerkleTree/Incremental.hs
+++ merkle-tree-incremental/src/Crypto/Hash/MerkleTree/Incremental.hs
@@ -1,34 +1,35 @@
 module Crypto.Hash.MerkleTree.Incremental (
     MerkleTree (..),
     MerkleHash,
-    merkleTreeHash,
-    leafHash,
-    nodeHash,
-    getMerkleRootHex,
+    merkleRootHash,
+    encodeMerkleHashHex,
+    empty,
+    add,
+    finalize,
 )
 where
 
 import Crypto.Hash (Digest, HashAlgorithm, hash, hashFinalize, hashInit, hashUpdate, hashUpdates)
-import Data.ByteArray as BA
+import Data.ByteArray (ByteArrayAccess)
 import Data.ByteArray.Encoding qualified as BA
 import Data.ByteString qualified as B
 
+-- ----------------------------------------------------------------
+-- Merkle Tree and Hashes
+
 type MerkleHash a = Digest a
 
-merkleHashHex :: (HashAlgorithm a) => MerkleHash a -> B.ByteString
-merkleHashHex = BA.convertToBase BA.Base16
+encodeMerkleHashHex :: (HashAlgorithm a) => MerkleHash a -> B.ByteString
+encodeMerkleHashHex = BA.convertToBase BA.Base16
 
 data MerkleTree a
     = MerkleTreeEmpty
     | MerkleTreeRoot (MerkleHash a)
     deriving (Show, Eq)
 
-getMerkleRootHex :: (HashAlgorithm a) => MerkleTree a -> B.ByteString
-getMerkleRootHex = merkleHashHex . merkleTreeHash
-
-merkleTreeHash :: forall a. (HashAlgorithm a) => MerkleTree a -> MerkleHash a
-merkleTreeHash MerkleTreeEmpty = hash B.empty
-merkleTreeHash (MerkleTreeRoot h) = h
+merkleRootHash :: forall a. (HashAlgorithm a) => MerkleTree a -> MerkleHash a
+merkleRootHash MerkleTreeEmpty = hash B.empty
+merkleRootHash (MerkleTreeRoot h) = h
 
 -- | Hash function to use for merkle tree
 leafHash :: forall a b. (HashAlgorithm a, ByteArrayAccess b) => b -> MerkleHash a
@@ -37,4 +38,35 @@ leafHash b =
 
 nodeHash :: forall a. (HashAlgorithm a) => MerkleHash a -> MerkleHash a -> MerkleHash a
 nodeHash h1 h2 =
-    hashFinalize $ flip hashUpdates [merkleHashHex h1, merkleHashHex h2] $ hashUpdate hashInit $ B.singleton 1
+    hashFinalize $ flip hashUpdates [encodeMerkleHashHex h1, encodeMerkleHashHex h2] $ hashUpdate hashInit $ B.singleton 1
+
+-- ----------------------------------------------------------------
+-- Incremental Merkle Tree construction
+
+newtype MerkleTreeState a = MerkleTreeState [MerkleTreeStateNode a]
+
+data MerkleTreeStateNode a = MerkleTreeStateNode
+    { cLevel :: Int
+    , cHash :: MerkleHash a
+    }
+    deriving (Show)
+
+empty :: (HashAlgorithm a) => a -> MerkleTreeState a
+empty _ = MerkleTreeState []
+
+add :: (HashAlgorithm a, ByteArrayAccess b) => MerkleTreeState a -> b -> MerkleTreeState a
+add (MerkleTreeState state) bytes =
+    join (MerkleTreeState (MerkleTreeStateNode{cLevel = 0, cHash = leafHash bytes} : state))
+
+join :: (HashAlgorithm a) => MerkleTreeState a -> MerkleTreeState a
+join (MerkleTreeState ((MerkleTreeStateNode level1 hash1) : (MerkleTreeStateNode level2 hash2) : xs))
+    | level1 == level2 = join (MerkleTreeState (MerkleTreeStateNode{cLevel = level1 + 1, cHash = nodeHash hash2 hash1} : xs))
+join state = state
+
+finalize :: (HashAlgorithm a) => MerkleTreeState a -> MerkleTree a
+finalize (MerkleTreeState []) = MerkleTreeEmpty
+finalize (MerkleTreeState [MerkleTreeStateNode _ merkleHash]) =
+    MerkleTreeRoot merkleHash
+finalize (MerkleTreeState (MerkleTreeStateNode level1 hash1 : node2@(MerkleTreeStateNode level2 hash2) : xs))
+    | level1 == level2 = finalize (MerkleTreeState (MerkleTreeStateNode (level1 + 1) (nodeHash hash2 hash1) : xs))
+    | otherwise = finalize (MerkleTreeState (MerkleTreeStateNode (level1 + 1) hash1 : node2 : xs))
--- merkle-tree-incremental/test/Spec.hs
+++ merkle-tree-incremental/test/Spec.hs
@@ -1,17 +1,16 @@
-import Crypto.Hash.Algorithms (SHA3_256)
+import Crypto.Hash.Algorithms (SHA3_256 (SHA3_256))
 import Crypto.Hash.MerkleTree (mkMerkleTree, mtHash)
-import Crypto.Hash.MerkleTree.Incremental (getMerkleRootHex)
-import Crypto.Hash.MerkleTree.Incremental.MerkleTreeState
+import Crypto.Hash.MerkleTree.Incremental (add, empty, encodeMerkleHashHex, finalize, merkleRootHash)
 import Data.ByteString.Char8 qualified as C
 
 import Test.QuickCheck
 
 prop_merkle_root_equal :: [String] -> Bool
 prop_merkle_root_equal entries =
-    (getMerkleRootHex merkleTree1) == (mtHash merkleTree2)
+    (encodeMerkleHashHex $ merkleRootHash merkleTree1) == (mtHash merkleTree2)
   where
     entries_bytes = map C.pack entries
-    state = foldl add (empty :: MerkleTreeState SHA3_256) entries_bytes
+    state = foldl add (empty SHA3_256) entries_bytes
     merkleTree1 = finalize state
     merkleTree2 = mkMerkleTree entries_bytes
 
