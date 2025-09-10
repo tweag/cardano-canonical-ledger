c80f3f5d0d7a4a50a6c5d039cf01031dbf8eadc6
Alexander Vershilov <alexander.vershilov@moduscreate.com>
2025-09-10 08:38:42 +0200

    Refactor: chage type of the serialize to accept Stream
    We change type of the reference implementation so it
    will match the type of the type of the real one.
    
    It will be easier to write tests and code this way,
    because we can drop-in replace implementations.

--- scls-format/examples/write-file/Main.hs
+++ scls-format/examples/write-file/Main.hs
@@ -9,6 +9,7 @@ import Data.ByteString qualified as BS
 import Data.ByteString.Lazy qualified as BSL
 import Data.Int (Int64)
 import Data.Text qualified as T
+import Streaming.Prelude qualified as S
 import System.Environment (getArgs)
 
 -- TODO: introduce a context for file generator
@@ -20,7 +21,7 @@ main = do
   [file_name] <- getArgs
   lazy_input <- BSL.getContents
   let chunks = chunksOf 1024 lazy_input
-  serialize file_name Mainnet (SlotNo 0xDEADBEEF) (T.pack "raw/v0") chunks
+  serialize file_name Mainnet (SlotNo 0xDEADBEEF) (T.pack "raw/v0") (S.each chunks)
 
 chunksOf :: Int64 -> BSL.ByteString -> [BS.ByteString]
 chunksOf chunk_size bs = do
