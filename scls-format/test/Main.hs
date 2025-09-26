a0b00f352b52729165e806e6527c804fd51b8b39
João Santos Reis <joao.reis@tweag.io>
2025-09-26 10:45:02 +0100

    Move rountrip test to its own module

--- scls-format/test/Main.hs
+++ scls-format/test/Main.hs
@@ -1,94 +1,14 @@
 module Main (main) where
 
-import Cardano.SCLS.CDDL (namespaces)
-import Cardano.SCLS.Internal.Hash (Digest (..))
-import Cardano.SCLS.Internal.Reader (extractRootHash, withNamespacedData)
-import Cardano.SCLS.Internal.Serializer.External.Impl qualified as External (serialize)
-import Cardano.SCLS.Internal.Serializer.MemPack
-import Cardano.SCLS.Internal.Serializer.Reference.Impl (InputChunk)
-import Cardano.SCLS.Internal.Serializer.Reference.Impl qualified as Reference (serialize)
-import Cardano.Types.Network (NetworkId (..))
-import Cardano.Types.SlotNo (SlotNo (..))
 import ChunksBuilderSpec (chunksBuilderTests)
-import Codec.CBOR.Cuddle.CBOR.Gen (generateCBORTerm')
-import Codec.CBOR.Cuddle.CDDL (Name (..))
-import Codec.CBOR.Cuddle.CDDL.Resolve (
-  asMap,
-  buildMonoCTree,
-  buildRefCTree,
-  buildResolvedCTree,
- )
-import Codec.CBOR.Cuddle.Huddle
-import Codec.CBOR.Term (encodeTerm)
-import Codec.CBOR.Write (toStrictByteString)
-import Control.Monad (replicateM)
-import Crypto.Hash.MerkleTree.Incremental qualified as MT
-import Data.Function ((&))
-import Data.List (sort)
-import Data.Map.Strict qualified as Map
-import Data.Text qualified as T
-import Streaming.Prelude qualified as S
-import System.FilePath ((</>))
-import System.IO.Temp (withSystemTempDirectory)
-import System.Random.Stateful (applyAtomicGen, globalStdGen)
-import Test.Hspec
-import Test.Hspec.Expectations.Contrib
-
 import MultiNamespace qualified (tests)
+import Roundtrip qualified (tests)
 
-type SerializeF = FilePath -> NetworkId -> SlotNo -> S.Stream (S.Of (InputChunk RawBytes)) IO () -> IO ()
+import Test.Hspec
 
 main :: IO ()
 main = do
   hspec $ do
-    tests
+    Roundtrip.tests
     chunksBuilderTests
     MultiNamespace.tests
- where
-  tests =
-    describe "Roundtrip test" $ do
-      mkRountripTestsFor "Reference" (Reference.serialize @RawBytes)
-      mkRountripTestsFor "External" (External.serialize @RawBytes)
-
-  mkRountripTestsFor :: String -> SerializeF -> Spec
-  mkRountripTestsFor groupName serialize =
-    describe groupName $ do
-      sequence_
-        [ context n $ it "should succeed with stream roundtrip" $ roundtrip (T.pack n) (toCDDL huddle) serialize
-        | (n, huddle) <- Map.toList namespaces
-        ]
-
-  roundtrip namespace cddl serialize = do
-    case buildMonoCTree =<< buildResolvedCTree (buildRefCTree $ asMap cddl) of
-      Left err -> expectationFailure $ "Failed to build CTree: " ++ show err
-      Right mt -> withSystemTempDirectory "scls-format-test-XXXXXX" $ \fn -> do
-        data_ <-
-          replicateM 1024 $
-            applyAtomicGen (generateCBORTerm' mt (Name (T.pack "record_entry") mempty)) globalStdGen
-        let encoded_data = [toStrictByteString (encodeTerm term) | term <- data_]
-        let fileName = (fn </> "data.scls")
-        _ <-
-          serialize
-            fileName
-            Mainnet
-            (SlotNo 1)
-            (S.each [(namespace S.:> (S.each encoded_data & S.map RawBytes))])
-        withNamespacedData
-          fileName
-          namespace
-          ( \stream -> do
-              decoded_data <- S.toList_ stream
-              annotate
-                "Stream roundtrip successful"
-                $ [b | RawBytes b <- decoded_data]
-                  `shouldBe` (sort encoded_data)
-          )
-        -- Check roundtrip of root hash
-        file_digest <- extractRootHash fileName
-        expected_digest <-
-          S.each (sort encoded_data)
-            & S.fold_ MT.add (MT.empty undefined) (Digest . MT.merkleRootHash . MT.finalize)
-        annotate
-          "Root hash roundtrip successful"
-          $ file_digest
-            `shouldBe` (Digest $ MT.merkleRootHash $ MT.finalize $ MT.add (MT.empty undefined) expected_digest)
