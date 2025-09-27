3d76713cede7e3c4f6cfc0e50ac6672e0f68f646
Alexander Vershilov <alexander.vershilov@moduscreate.com>
2025-09-27 17:35:11 +0200

    Add instances for MemPack using ByteOrder

--- scls-format/src/Cardano/Types/SlotNo.hs
+++ scls-format/src/Cardano/Types/SlotNo.hs
@@ -4,10 +4,12 @@
 module Cardano.Types.SlotNo (SlotNo (..)) where
 
 import Cardano.Types.ByteOrdered
+import Data.MemPack
 import Foreign
 
 -- TODO: define mempack or another class type for data serialisation
 
 newtype SlotNo = SlotNo {unSlotNo :: Word64}
-  deriving (Eq, Ord, Show, Read)
+  deriving (Ord, Eq, Show, Read)
   deriving (Storable) via (BigEndian Word64)
+  deriving (MemPack) via (BigEndian Word64)
