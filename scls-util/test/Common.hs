e271c8af1b98ea4d0aba51da9ca472e54a230dee
João Santos Reis <joao.reis@tweag.io>
2025-10-13 14:05:53 +0100

    Refactor test suite modules

---
+++ scls-util/test/Common.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Common (generateTestFile, runSclsUtil) where
+
+import Cardano.SCLS.Internal.Serializer.MemPack (RawBytes (..))
+import Cardano.SCLS.Internal.Serializer.Reference.Impl qualified as Reference
+import Cardano.Types.Network (NetworkId (Mainnet))
+import Cardano.Types.SlotNo (SlotNo (SlotNo))
+import Data.ByteString.Char8 qualified as BS8
+import Data.Text (Text)
+import Streaming qualified as S
+import Streaming.Prelude qualified as S
+import System.Exit (ExitCode (..))
+import System.FilePath ((</>))
+import System.Process (readProcessWithExitCode)
+
+generateTestFile :: FilePath -> IO (FilePath, [Text])
+generateTestFile dir = do
+  let fileName = dir </> "test.scls"
+      testData =
+        [ ("namespace1", [BS8.pack (show i) | i <- [1 :: Int .. 100]])
+        , ("namespace2", [BS8.pack (show i) | i <- [1 :: Int .. 50]])
+        , ("namespace3", [BS8.pack (show i) | i <- [1 :: Int .. 75]])
+        ]
+      mkStream =
+        S.each
+          [ ns S.:> S.each (map RawBytes entries)
+          | (ns, entries) <- testData
+          ]
+
+  Reference.serialize @RawBytes
+    fileName
+    Mainnet
+    (SlotNo 1)
+    mkStream
+
+  return (fileName, map fst testData)
+
+runSclsUtil :: [String] -> IO (ExitCode, String, String)
+runSclsUtil args = do
+  readProcessWithExitCode "cabal" (["run", "scls-util", "--"] ++ args) ""
---
+++ scls-util/test/VerifySpec.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeApplications #-}
+
+module VerifySpec (verifyCommandTests, verifyNsCommandTests) where
+
+import Cardano.SCLS.Internal.Reader (
+  extractNamespaceHash,
+  extractRootHash,
+ )
+import Common
+import Control.Monad (forM_)
+import Data.Text qualified as T
+import System.Exit (ExitCode (..))
+import System.IO.Temp (withSystemTempDirectory)
+import Test.Hspec
+
+verifyCommandTests :: Spec
+verifyCommandTests = describe "verify command" do
+  it "verifies a valid SCLS file" do
+    withSystemTempDirectory "scls-util-test-XXXXXX" \dir -> do
+      (fileName, _) <- generateTestFile dir
+      (exitCode, stdout, _) <- runSclsUtil ["verify", fileName]
+      h <- extractRootHash fileName
+
+      exitCode `shouldBe` ExitSuccess
+
+      stdout `shouldContain` (show h)
+
+  it "fails for non-existent file" do
+    (exitCode, _, _) <- runSclsUtil ["verify", "/nonexistent/file.scls"]
+
+    exitCode `shouldBe` ExitFailure 1
+
+verifyNsCommandTests :: Spec
+verifyNsCommandTests = describe "verify-ns command" do
+  it "verifies all namespaces correctly" do
+    withSystemTempDirectory "scls-util-test-XXXXXX" \dir -> do
+      (fileName, namespaces) <- generateTestFile dir
+
+      forM_ namespaces \ns -> do
+        (exitCode, stdout, _) <- runSclsUtil ["verify-ns", fileName, T.unpack ns]
+        Just h <- extractNamespaceHash ns fileName
+
+        exitCode `shouldBe` ExitSuccess
+
+        stdout `shouldContain` (show h)
+
+  it "fails for non-existent namespace" do
+    withSystemTempDirectory "scls-util-test-XXXXXX" \dir -> do
+      (fileName, _) <- generateTestFile dir
+      (exitCode, _, _) <- runSclsUtil ["verify-ns", fileName, "nonexistent"]
+
+      exitCode `shouldBe` ExitFailure 65
