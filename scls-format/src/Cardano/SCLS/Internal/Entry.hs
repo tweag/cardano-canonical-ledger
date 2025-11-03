195658e075b7060011c31d24140e01397bf93621
Alexander Vershilov <alexander.vershilov@moduscreate.com>
2025-11-03 12:21:59 +0100

    Introduce GenericCBOREntry for reading the values on the unknown type
    Introdunce two new types:
    - GenericCBOREntry for storing and reading any CBOR Term stored in utxo
    - SomeCBOREntry that is used only for generation, and allows to bypass
      the restriction that the types on all entries in the Plan should be the
      same. This restriction is problematic because namespaces should be able
      to use differently sized keys and as a result differrent types

--- scls-format/src/Cardano/SCLS/Internal/Entry.hs
+++ scls-format/src/Cardano/SCLS/Internal/Entry.hs
@@ -1,18 +1,22 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Cardano.SCLS.Internal.Entry (
   IsKey (..),
   ChunkEntry (..),
+  GenericCBOREntry (..),
+  SomeCBOREntry (..),
 ) where
 
 import Cardano.SCLS.Internal.Serializer.HasKey
-import Cardano.SCLS.Internal.Serializer.MemPack (MemPackHeaderOffset (..))
+import Cardano.SCLS.Internal.Serializer.MemPack (ByteStringSized (..), CBORTerm (..), MemPackHeaderOffset (..), isolated)
 import Cardano.Types.ByteOrdered (BigEndian (..))
 import Data.MemPack
 import Data.MemPack.Buffer
 import Data.Typeable
 import Data.Word (Word32)
+import GHC.TypeLits (KnownNat, Nat, natVal)
 
 class (Ord a) => IsKey a where
   keySize :: Int
@@ -50,3 +54,46 @@ instance (Eq k, Eq v) => Eq (ChunkEntry k v) where
 
 instance (Ord k, Ord v) => Ord (ChunkEntry k v) where
   compare (ChunkEntry k1 v1) (ChunkEntry k2 v2) = compare k1 k2 <> compare v1 v2
+
+{- | A generic chunk entry that we are not aware of exact content type,
+except that it is a valid CBOR, as required per SCLS specification.
+-}
+newtype GenericCBOREntry (n :: Nat) = GenericCBOREntry
+  { unGenericCBOREntry :: ChunkEntry (ByteStringSized n) CBORTerm
+  }
+  deriving (Show, Eq, Ord)
+
+instance HasKey (GenericCBOREntry n) where
+  type Key (GenericCBOREntry n) = ByteStringSized n
+  getKey (GenericCBOREntry (ChunkEntry k _)) = k
+
+instance (KnownNat n) => IsKey (ByteStringSized n) where
+  keySize = fromInteger (natVal (Proxy :: Proxy n))
+  packKeyM = packM
+  unpackKeyM = unpackM
+
+instance (KnownNat n, Typeable (ByteStringSized n)) => MemPack (GenericCBOREntry n) where
+  packedByteCount (GenericCBOREntry ce) = packedByteCount ce
+  packM (GenericCBOREntry ce) = packM ce
+  unpackM =
+    GenericCBOREntry <$> do
+      BigEndian (lenEntry :: Word32) <- unpackM
+      key <- unpackKeyM
+      value <- isolated (fromIntegral lenEntry - fromIntegral (keySize @(ByteStringSized n)))
+      return (ChunkEntry key value)
+
+instance (KnownNat n) => MemPackHeaderOffset (GenericCBOREntry n) where
+  headerSizeOffset = 4
+
+{- | An existential wrapper that allows to store 'GenericCBOREntry' of any
+size in the same plan.
+-}
+data SomeCBOREntry = forall n. (KnownNat n) => SomeCBOREntry (GenericCBOREntry n)
+
+instance MemPack SomeCBOREntry where
+  packedByteCount (SomeCBOREntry gce) = packedByteCount gce
+  packM (SomeCBOREntry gce) = packM gce
+  unpackM = error "unpackM SomeCBOREntry: cannot determine size n at runtime"
+
+instance MemPackHeaderOffset SomeCBOREntry where
+  headerSizeOffset = 4
