f9caaba07452732de93a457b2e0033084b87ad3b
João Santos Reis <joao.reis@tweag.io>
2025-11-04 14:25:35 +0000

    Add docs

--- scls-format/src/Cardano/SCLS/Query.hs
+++ scls-format/src/Cardano/SCLS/Query.hs
@@ -9,11 +9,21 @@ import Data.MemPack (MemPack)
 import Data.Typeable (Typeable)
 import Streaming.Prelude qualified as S
 
+-- | Query for a specific entry by namespace and key.
+--
+-- This function searches for an entry with the given key within the specified
+-- namespace in the SCLS data directory. It leverages the assumption that entries
+-- are stored in sorted order by key, allowing for early termination once a key
+-- greater than or equal to the target is found.
 queryEntry ::
   (HasKey a, MemPack a, Typeable a) =>
-  FilePath ->
+  -- | The directory path containing the SCLS data files
+  FilePath ->  
+  -- | The namespace to search within
   Namespace ->
+  -- | The key of the entry to find
   Key a ->
+  -- | Returns @Just entry@ if an entry with the exact key is found, @Nothing@ otherwise
   IO (Maybe a)
 queryEntry dir ns key = do
   withNamespacedData dir ns $ \stream -> do
