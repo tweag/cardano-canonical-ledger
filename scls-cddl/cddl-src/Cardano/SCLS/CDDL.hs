0ee75bbbf33c344d02a253ab63a26c0fe05d9fc5
Alexander Vershilov <alexander.vershilov@moduscreate.com>
2025-11-14 12:25:14 +0100

    Introduce snapshots namespace
    Introduce snapshot namespace by following CCDL in cardano-ledger
    specs. There are few minir differences to how encoding is described
    in the spec. For the sum-types tag is moved to the rule that describes
    variants and variants them-selves are described as a group without tag.

--- scls-cddl/cddl-src/Cardano/SCLS/CDDL.hs
+++ scls-cddl/cddl-src/Cardano/SCLS/CDDL.hs
@@ -7,6 +7,7 @@ module Cardano.SCLS.CDDL (
 
 import Cardano.SCLS.Namespace.Blocks qualified as Blocks
 import Cardano.SCLS.Namespace.Pots qualified as Pots
+import Cardano.SCLS.Namespace.Snapshots qualified as Snapshots
 import Cardano.SCLS.Namespace.UTxO qualified as UTxO
 import Codec.CBOR.Cuddle.Huddle (Huddle, HuddleItem (HIRule), collectFromInit)
 import Data.Map.Strict qualified as Map
@@ -28,4 +29,5 @@ namespaces =
     [ ("utxo/v0", NamespaceInfo (collectFromInit [HIRule UTxO.record_entry]) 34)
     , ("blocks/v0", NamespaceInfo (collectFromInit [HIRule Blocks.record_entry]) 36) -- 28 bytes for key, and 8 for epoch in BE
     , ("pots/v0", NamespaceInfo (collectFromInit [HIRule Pots.record_entry]) 8) -- Key is epoch number
+    , ("snapshots/v0", NamespaceInfo (collectFromInit [HIRule Snapshots.record_entry]) 30)
     ]
