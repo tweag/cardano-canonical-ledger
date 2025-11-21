1c0014e9e9cc1e7eedbca8f62c478b5dd6b4f905
Alexander Vershilov <alexander.vershilov@moduscreate.com>
2025-11-22 00:32:59 +0100

    missing

---
+++ scls-cddl/cddl-src/Cardano/SCLS/Namespace/GovCommitee.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Use camelCase" #-}
+module Cardano.SCLS.Namespace.GovCommittee where
+
+import Cardano.SCLS.Common
+import Codec.CBOR.Cuddle.Huddle
+import Data.Function (($))
+import Text.Heredoc (str)
+
+record_entry :: Rule
+record_entry =
+  comment
+    [str| The key for the entry is  epoch number (8 bytes) |]
+    $ "record_entry" =:= committee
+
+committee_cold_credential :: Rule
+committee_cold_credential = "committee_cold_credential" =:= credential
+
+committee :: Rule
+committee =
+  "committee"
+    =:= arr
+      [ a (mp [0 <+ asKey committee_cold_credential ==> epoch_no])
+      , a unit_interval
+      ]
