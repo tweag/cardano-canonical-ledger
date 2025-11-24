77a65db8aea80f3a25b0066af0b4ce9f4d8684f0
Alexander Vershilov <alexander.vershilov@moduscreate.com>
2025-11-24 16:11:12 +0100

    cuddle

--- scls-format/mempack-1.0/Data/MemPack/ByteOrdered.hs
+++ scls-format/mempack-1.0/Data/MemPack/ByteOrdered.hs
@@ -1,10 +1,20 @@
 -- | Helpers for dealing with endianness in binary formats.
 module Data.MemPack.ByteOrdered (
-  BigEndian (..),
   Storable (..),
+
+  -- * Big endian
+  BigEndian (..),
+  packWord16beM,
   packWord32beM,
   packWord64beM,
   unpackBigEndianM,
+
+  -- * Little endian
+  LittleEndian (..),
+  packWord16leM,
+  packWord32leM,
+  packWord64leM,
+  unpackLittleEndianM,
 ) where
 
 import Data.MemPack
@@ -28,6 +38,9 @@ newtype SlotNo = SlotNo { unSlotNo :: Word64 }
 newtype BigEndian a = BigEndian {unBigEndian :: a}
   deriving (Eq, Ord, Show, Read)
 
+newtype LittleEndian a = LittleEndian {unLittleEndian :: a}
+  deriving (Eq, Ord, Show, Read)
+
 instance (Bytes a, Storable a) => Storable (BigEndian a) where
   sizeOf _ = sizeOf (undefined :: a)
   alignment _ = alignment (undefined :: a)
@@ -36,14 +49,31 @@ instance (Bytes a, Storable a) => Storable (BigEndian a) where
     return (BigEndian (fromBigEndian w))
   poke ptr (BigEndian w) = poke (castPtr ptr :: Ptr a) (toBigEndian w)
 
+instance (Bytes a, Storable a) => Storable (LittleEndian a) where
+  sizeOf _ = sizeOf (undefined :: a)
+  alignment _ = alignment (undefined :: a)
+  peek ptr = do
+    w <- peek (castPtr ptr :: Ptr a)
+    return (LittleEndian (fromLittleEndian w))
+  poke ptr (LittleEndian w) = poke (castPtr ptr :: Ptr a) (toLittleEndian w)
+
 instance (Bytes a, MemPack a) => MemPack (BigEndian a) where
   typeName = "BigEndian " ++ typeName @a
   packedByteCount (BigEndian a) = packedByteCount a
   packM (BigEndian a) = packM (toBigEndian a)
   unpackM = BigEndian . fromBigEndian <$> unpackM
 
+instance (Bytes a, MemPack a) => MemPack (LittleEndian a) where
+  typeName = "LittleEndian " ++ typeName @a
+  packedByteCount (LittleEndian a) = packedByteCount a
+  packM (LittleEndian a) = packM (toLittleEndian a)
+  unpackM = LittleEndian . fromLittleEndian <$> unpackM
+
 packBigEndianM :: (Bytes a, MemPack a) => a -> Pack s ()
 packBigEndianM = packM . BigEndian
+
+packWord16beM :: Word16 -> Pack s ()
+packWord16beM = packBigEndianM
 
 packWord32beM :: Word32 -> Pack s ()
 packWord32beM = packBigEndianM
@@ -54,3 +84,19 @@ packWord64beM = packBigEndianM
 unpackBigEndianM :: (Bytes a, MemPack a, Buffer b) => Unpack b a
 unpackBigEndianM =
   unBigEndian <$> unpackM
+
+packLittleEndianM :: (Bytes a, MemPack a) => a -> Pack s ()
+packLittleEndianM = packM . LittleEndian
+
+packWord16leM :: Word16 -> Pack s ()
+packWord16leM = packLittleEndianM
+
+packWord32leM :: Word32 -> Pack s ()
+packWord32leM = packLittleEndianM
+
+packWord64leM :: Word64 -> Pack s ()
+packWord64leM = packLittleEndianM
+
+unpackLittleEndianM :: (Bytes a, MemPack a, Buffer b) => Unpack s b a
+unpackLittleEndianM =
+  unLittleEndian <$> unpackM
