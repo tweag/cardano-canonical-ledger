1bec4e86c7ba7c794086aeaab0c64eab5e77290a
Alexander Vershilov <alexander.vershilov@moduscreate.com>
2025-12-03 16:38:59 +0100

    gov/proposals/v0: introduce protocol params update
    Each field in the protocol parameters update may be missing.
    Current spec allows that.

--- scls-cddl/cddl-src/Cardano/SCLS/Namespace/GovProposals.hs
+++ scls-cddl/cddl-src/Cardano/SCLS/Namespace/GovProposals.hs
@@ -55,7 +55,7 @@ gov_action =
     =:= ( arr
             [ 0
             , "purpose" ==> (gov_action_id / VNil)
-            , "update" ==> gov_pparams_out
+            , "update" ==> gov_params_update
             , "hash" ==> (script_hash / VNil)
             ]
             //- "Params update"
@@ -85,3 +85,64 @@ gov_action_id :: Rule
 gov_action_id =
   "gov_action_id"
     =:= arr ["transaction_id" ==> hash32, "gov_action_index" ==> VUInt `sized` (2 :: Word64)]
+
+gov_params_update :: Rule
+gov_params_update =
+  comment
+    [str| Governance protocol parameters
+              |   - min_fee_a: the linear factor for the minimum fee calculation
+              |   - min_fee_b: the constant factor for the minimum fee calculation
+              |   - max_block_size: maximal block body size in bytes
+              |   - max_block_header_size: maximal block header size in bytes
+              |   - max_tx_size: maximal transaction size in bytes
+              |   - key_deposit: The amount of a key registration deposit
+              |   - pool_deposit: The amount of a pool registration deposit
+              |   - epoch_max: epoch bound on pool retirement
+              |   - n_opt: desired number of pools
+              |   - a0: pool influence factor
+              |   - rho: monetary expansion
+              |   - tau: treasury expansion
+              |   - d: decentralisation parameter
+              |   - min_pool_cost: minimum pool cost
+              |   - min_utxo_value: Minimum Lovelace in a UTxO deprecated by AdaPerUTxOWord
+              |   - ada_per_utxo_byte: Cost in ada per 1 byte of UTxO storage instead of _coinsPerUTxOWord
+              |   - cost_models: Cost models for non-native script languages
+              |   - prices: Prices of execution units for non-native script languages
+              |   - max_tx_ex_units: Max total script execution resources units allowed per tx
+              |   - max_block_ex_units: Max total script execution resources units allowed per block
+              |   - max_value_size: Max size of a Value in an output
+              |   - collateral_percentage: The scaling percentage of the collateral relative to the fee
+              |   - max_collateral_inputs: Maximum number of collateral inputs allowed in a transaction
+              |]
+    $ "gov_params_update"
+      =:= mp
+        [ "a0" ==> (nonnegative_interval / VNil)
+        , "rho" ==> (unit_interval / VNil)
+        , "tau" ==> (unit_interval / VNil)
+        , "n_opt" ==> (VUInt `sized` (2 :: Word64) / VNil)
+        , "prices" ==> (ex_unit_prices / VNil)
+        , "epoch_max" ==> (epoch_interval / VNil)
+        , "min_fee_a" ==> (coin / VNil)
+        , "min_fee_b" ==> (coin / VNil)
+        , "cost_models" ==> (cost_models / VNil)
+        , "key_deposit" ==> (coin / VNil)
+        , "max_tx_size" ==> (VUInt `sized` (4 :: Word64) / VNil)
+        , "drep_deposit" ==> (coin / VNil)
+        , "pool_deposit" ==> (coin / VNil)
+        , "max_val_size" ==> (VUInt `sized` (4 :: Word64) / VNil)
+        , "drep_activity" ==> (epoch_interval / VNil)
+        , "min_pool_cost" ==> (coin / VNil)
+        , "max_block_size" ==> (VUInt `sized` (4 :: Word64) / VNil)
+        , "max_tx_ex_units" ==> (ex_units / VNil)
+        , "coin_per_utxo_byte" ==> (coin / VNil)
+        , "gov_action_deposit" ==> (coin / VNil)
+        , "max_block_ex_units" ==> (ex_units / VNil)
+        , "min_committee_size" ==> (VUInt `sized` (2 :: Word64) / VNil)
+        , "committee_term_limit" ==> (epoch_interval / VNil)
+        , "collateral_percentage" ==> (VUInt `sized` (2 :: Word64) / VNil)
+        , "drep_voting_thresholds" ==> (drep_voting_thresholds / VNil)
+        , "gov_action_lifetime" ==> (epoch_interval / VNil)
+        , "max_block_header_size" ==> (VUInt `sized` (2 :: Word64) / VNil)
+        , "max_collateral_inputs" ==> (VUInt `sized` (2 :: Word64) / VNil)
+        , "pool_voting_thresholds" ==> (pool_voting_thresholds / VNil)
+        ]
