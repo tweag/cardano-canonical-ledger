4165460f60476aac9840f6e134e28cac7196fe4d
João Santos Reis <joao.reis@tweag.io>
2025-12-10 21:09:57 +0000

    Fix typo

--- scls-core/src/Cardano/SCLS/CBOR/Canonical.hs
+++ scls-core/src/Cardano/SCLS/CBOR/Canonical.hs
@@ -13,7 +13,7 @@ newtype CanonicalEncoding = CanonicalEncoding {unCanonicalEncoding :: Encoding}
   deriving (Semigroup, Monoid)
 
 {- | Unsafe lifting `Encoding` to `CanonicalEncoding`. Does not ensure that `Encoding` was defined according to canonical rules.
-It's the user responsability to ensure the underlying encoding follows deterministic rules.
+It's the user responsibility to ensure the underlying encoding follows deterministic rules.
 -}
 unsafeToCanonicalEncoding :: Encoding -> CanonicalEncoding
 unsafeToCanonicalEncoding = CanonicalEncoding
@@ -23,7 +23,7 @@ newtype CanonicalDecoder s a = CanonicalDecoder {unCanonicalDecoder :: Decoder s
   deriving (Functor, Applicative, Monad, MonadFail)
 
 {- | Unsafe lifting `Decoder` to `CanonicalDecoder`. Does not ensure that `Decoder` was defined according to canonical rules.
-It's the user responsability to ensure the underlying decoder follows deterministic rules.
+It's the user responsibility to ensure the underlying decoder follows deterministic rules.
 -}
 unsafeToCanonicalDecoder :: Decoder s a -> CanonicalDecoder s a
 unsafeToCanonicalDecoder = CanonicalDecoder
