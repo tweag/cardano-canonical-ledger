1beb5176f2f5ac3052994da1942f6f115af3e274
João Santos Reis <joao.reis@tweag.io>
2025-12-15 09:34:41 +0000

    Use validateCBOR
    validateTerm is not exported upstream anymore

--- scls-cardano/cddl-validate/Cardano/SCLS/CDDL/Validate.hs
+++ scls-cardano/cddl-validate/Cardano/SCLS/CDDL/Validate.hs
@@ -3,13 +3,12 @@
 
 -- | Various helper functions for CBOR validation against supported CDDL specifications.
 module Cardano.SCLS.CDDL.Validate (
-  validateTermAgainst,
   validateBytesAgainst,
   invalidSpecs,
 ) where
 
 import Cardano.SCLS.CDDL
-import Codec.CBOR.Cuddle.CBOR.Validator (CBORTermResult (..), validateTerm)
+import Codec.CBOR.Cuddle.CBOR.Validator (CBORTermResult (..), validateCBOR)
 import Codec.CBOR.Cuddle.CDDL (Name (..))
 import Codec.CBOR.Cuddle.CDDL.CTree (CTreeRoot (..))
 import Codec.CBOR.Cuddle.CDDL.Resolve (
@@ -22,12 +21,7 @@ import Codec.CBOR.Cuddle.CDDL.Resolve (
  )
 import Codec.CBOR.Cuddle.Huddle (toCDDL)
 import Codec.CBOR.Cuddle.IndexMappable (IndexMappable (mapIndex), mapCDDLDropExt)
-import Codec.CBOR.Read qualified as CBOR
-import Codec.CBOR.Term (Term, decodeTerm)
-import Control.Monad.Trans.Reader (runReader)
 import Data.ByteString (ByteString)
-import Data.ByteString.Lazy qualified as BSL
-import Data.Functor ((<&>))
 import Data.Map.Strict qualified as Map
 import Data.Text (Text)
 
@@ -42,17 +36,8 @@ validSpecs :: Map.Map Text (CTreeRoot Codec.CBOR.Cuddle.CDDL.Resolve.MonoReferen
         Right tree -> Right tree
   do namespaces
 
--- | Validate a parsed CBOR term against a rule in the namespace.
-validateTermAgainst :: Term -> Text -> Text -> Maybe CBORTermResult
-validateTermAgainst term namespace name =
-  let cddlName = Name name
-   in Map.lookup namespace validSpecs >>= \cddl@(CTreeRoot cddlTree) ->
-        Map.lookup cddlName cddlTree <&> \rule ->
-          runReader (validateTerm term (runIdentity rule)) cddl
-
 -- | Validate raw bytes against a rule in the namespace.
 validateBytesAgainst :: ByteString -> Text -> Text -> Maybe CBORTermResult
-validateBytesAgainst bytes namespace name =
-  case CBOR.deserialiseFromBytes decodeTerm (BSL.fromStrict bytes) of
-    Left _ -> Nothing
-    Right (_, term) -> validateTermAgainst term namespace name
+validateBytesAgainst bytes namespace name = do
+  cddl <- Map.lookup namespace validSpecs
+  pure $ validateCBOR bytes (Name name) (mapIndex cddl)
